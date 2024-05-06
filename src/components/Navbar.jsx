0b79dc8caf3cafc2e27cc503071d7d8accf69959
logprose <logprose@umich.edu>
2024-05-06 15:06:28 -0400

    Navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
-import styles from '../styles/components/Navbar.css'
+import styles from "../styles/components/Navbar.module.css"
 
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
       <div className={styles.container}>
-        <a href="/">Home</a>
-        <a href="/User">User Account</a>
-        <a href="/Saved">Saved Locations</a>
+        <p>Ann Arbor Eats!</p>
+        <a className={styles.home} href="/"/>
+        <a className={styles.user} href="/User"/>
+        <a className={styles.location} href="/Saved"/>
       </div>
     </div>
   );
