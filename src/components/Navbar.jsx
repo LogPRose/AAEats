e4cda1a4ead22a9fb30d1c0151b6ebca9788ab47
logprose <logprose@umich.edu>
2024-05-06 15:55:27 -0400

    Fixed styling for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,9 @@ const Navbar = () => {
     <div className={styles.navbar}>
       <div className={styles.container}>
         <p>Ann Arbor Eats!</p>
-        <a className={styles.home} href="/"/>
-        <a className={styles.user} href="/User"/>
-        <a className={styles.location} href="/Saved"/>
+        <a alt="Home" className={styles.home} href="/"/>
+        <a alt="User" className={styles.user} href="/User"/>
+        <a alt="Saved Locations" className={styles.location} href="/Saved"/>
       </div>
     </div>
   );
