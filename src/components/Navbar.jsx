ff34a8939a0afe4b3fe6809bdb34bcf371f58868
logprose <logprose@umich.edu>
2024-05-07 14:34:57 -0400

    Event handler for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
 import styles from "../styles/components/Navbar.module.css"
 
+const [active, useState] = useState('home')
+
+const handleRoute = (Route) => {
+    active = Route;
+    navigate("/${Route}");
+}
+
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
       <div className={styles.container}>
         <p>Ann Arbor Eats!</p>
-        <a alt="Home" className={styles.home} href="/"/>
-        <a alt="User" className={styles.user} href="/User"/>
-        <a alt="Saved Locations" className={styles.location} href="/Saved"/>
+        <input alt="Home" type="image" src={styles.home} onClick={handleRoute('home')}/>
+        <input alt="User Account" type="image" src={styles.user} onClick={handleRoute('user')}/>
+        <input alt="Saved Locations" type="image" src={styles.location} onClick={handleRoute('location')}/>
       </div>
     </div>
   );
