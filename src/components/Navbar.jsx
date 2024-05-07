72d1f40dd348df3581641eb9e5236f688331aadb
logprose@umich.edu <user@LoganPC>
2024-05-07 17:26:09 -0400

    Routing for navbar, need to fix styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/components/Navbar.module.css"
 
-const [active, useState] = useState('home')
-
-const handleRoute = (Route) => {
-    active = Route;
-    navigate("/${Route}");
-}
 
 const Navbar = () => {
+    const [route, setRoute] = useState(window.location.pathname);
+
+    useEffect(() => {
+        const handleRouteChange = () => {
+            setRoute(window.location.pathname);
+        }
+
+        window.addEventListener('popstate', handleRouteChange);
+
+        return () => {
+            window.removeEventListener('popstate', handleRouteChange);
+        }
+    }, []);
+    const handleRoute = (newRoute) => {
+        setRoute(newRoute);
+    }
+//add ternary for style switching when active
   return (
     <div className={styles.navbar}>
       <div className={styles.container}>
         <p>Ann Arbor Eats!</p>
-        <input alt="Home" type="image" src={styles.home} onClick={handleRoute('home')}/>
-        <input alt="User Account" type="image" src={styles.user} onClick={handleRoute('user')}/>
-        <input alt="Saved Locations" type="image" src={styles.location} onClick={handleRoute('location')}/>
+        <input alt="Home" type="image" src={styles.home} onClick={() => handleRoute('/')}/>
+        <input alt="User Account" type="image" src={styles.user} onClick={() => handleRoute('/user')}/> 
+        <input alt="Saved Locations" type="image" src={styles.location} onClick={() => handleRoute('/location')}/>
       </div>
     </div>
   );
 };
+
 
 export default Navbar;
